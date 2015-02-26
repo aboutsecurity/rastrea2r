8b5c215992aa8cffe6dddee283a48983f09d08c8
Ismael Valenzuela <ismael.valenzuela@gmail.com>
2015-02-26 18:21:03 -0500

    Changes to win32

--- win32/rastrea2r_win32_v0.3.py
+++ win32/rastrea2r_win32_v0.3.py
@@ -8,20 +8,27 @@
 import os
 import sys
 import yara
-import wmi
+import psutil # New multiplatform library
+import subprocess
+
+from time import gmtime, strftime
 from requests import post
 from argparse import ArgumentParser
 
 
+
 __author__ = 'ismael.valenzuela@foundstone.com'
-__version__ = '0.1'
+__version__ = '0.3'
+
+# added functionality to acquire a memory dump remotely using Winpmem
+# added triage module to wrap Sysinternals/third-party tools and Win native commands
 
 
 """ Variables """
 
 server_port = 8080 # Default REST server port
 
-def fetchyararule(server,rule):
+def fetchyararule(server, rule):
 
     """ Fetches yara rule from REST server"""
 
@@ -37,7 +44,7 @@ def fetchyararule(server,rule):
         return r.text
 
 
-def yaradisk(path,server,rule,silent):
+def yaradisk(path, server, rule, silent):
 
     """ Yara file/directory object scan module """
 
@@ -70,7 +77,7 @@ def yaradisk(path,server,rule,silent):
                 continue
 
 
-def yaramem(server,rule,silent):
+def yaramem(server, rule, silent):
 
     """ Yara process memory scan module """
 
@@ -83,16 +90,22 @@ def yaramem(server,rule,silent):
         print '\nScanning running processes in memory\n'
 
     mypid=os.getpid()
-    c=wmi.WMI()
 
     rule_bin=yara.compile(source=rule_text)
 
-    for process in c.win32_Process():
-        client_pid=process.ProcessId
-        client_pname=process.Name
-        client_ppath=process.ExecutablePath
-        client_pcmd=process.CommandLine
-        client_powner=process.GetOwner()[2]
+    for process in psutil.process_iter():
+        try:
+            pinfo = process.as_dict(attrs=['pid','name','exe','cmdline'])
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            if not silent:
+                print(pinfo)
+
+        client_pid=pinfo['pid']
+        client_pname=pinfo['name']
+        client_ppath=pinfo['exe']
+        client_pcmd=pinfo['cmdline']
 
         if client_pid!=mypid:
             try:
@@ -114,9 +127,64 @@ def yaramem(server,rule,silent):
                 p=post('http://'+server+':'+str(server_port)+'/putpid',data=payload)
 
 
+def memdump(tool_server, output_server, silent):
+
+    """ Memory acquisition module """
+
+    smb_bin=tool_server + r'\tools' # TOOLS Read-only share with third-party binary tools
+    smb_data=output_server + r'\data' # DATA Write-only share for output data
+
+    tool=('winpmem -') # Sends output to STDOUT
+
+    fullcommand=tool.split()
+    commandname=fullcommand[0].split('.')
+
+    recivedt=strftime('%Y%m%d%H%M%S', gmtime()) # Timestamp in GMT
+
+    f=open(r'\\'+smb_data+r'\\'+recivedt+'-'+os.environ['COMPUTERNAME']+'-'+commandname[0]+'.img','w')
+
+    if not silent:
+        print '\nDumping memory to ' +r'\\'+smb_data+r'\\'+recivedt+'-'+os.environ['COMPUTERNAME']+'-'\
+              +commandname[0]+'.img\n'
+
+    pst = subprocess.call(r'\\'+smb_bin+r'\\'+tool, stdout=f)
+
+
+def triage(tool_server, output_server, silent):
+
+    """ Triage collection module """
+
+    smb_bin=tool_server + r'\tools' # TOOLS Read-only share with third-party binary tools
+    smb_data=output_server + r'\data' # DATA Write-only share for output data
+
+    """ Add your list of Sysinternal / third-party / BATCH files here """
+
+    tool=('psloggedon.exe /accepteula',
+         'autorunsc.exe -a * -ct -h /accepteula',
+         'netstat.bat')
+
+    """ BATCH files must be called with the .bat extension """
+
+    for task in tool: # Iterates over the list of commands
+
+        fullcommand=task.split()
+        commandname=fullcommand[0].split('.')
+
+        recivedt=strftime('%Y%m%d%H%M%S', gmtime()) # Timestamp in GMT
+
+        f=open(r'\\'+smb_data+r'\\'+recivedt+'-'+os.environ['COMPUTERNAME']+'-'+commandname[0]+'.log','w')
+
+        if not silent:
+            print '\nSaving output of ' +task+ ' to '+r'\\'+smb_data+r'\\'+recivedt+'-'+os.environ['COMPUTERNAME']\
+                  +'-'+commandname[0]+'.log\n'
+
+        pst = subprocess.call(r'\\'+smb_bin+r'\\'+task, stdout=f)
+
+
 def main():
 
-    parser = ArgumentParser(description='Rastrea2r RESTful remote Yara/Triage tool for Incident Responders')
+    parser = ArgumentParser(description='::Rastrea2r RESTful remote Yara/Triage tool for Incident Responders '
+                                        'by Ismael Valenzuela @aboutsecurity / Foundstone (Intel Security)::')
 
     subparsers = parser.add_subparsers(dest="mode", help='modes of operation')
 
@@ -136,9 +204,18 @@ def main():
     list_parser.add_argument('rule', action='store', help='Yara rule on REST server')
     list_parser.add_argument('-s', '--silent', action='store_true', help='Suppresses standard output')
 
+    """Memory acquisition mode"""
+
+    list_parser = subparsers.add_parser('memdump', help='Acquires a memory dump from the endpoint')
+    list_parser.add_argument('BIN_server', action='store', help='Binary tool server (SMB share)')
+    list_parser.add_argument('DATA_server', action='store', help='Data output server (SMB share)')
+    list_parser.add_argument('-s', '--silent', action='store_true', help='Suppresses standard output')
+
     """Triage mode"""
 
-    list_parser = subparsers.add_parser('triage', help='Collect triage information from endpoint')
+    list_parser = subparsers.add_parser('triage', help='Collects triage information from the endpoint')
+    list_parser.add_argument('BIN_server', action='store', help='Binary tool server (SMB share)')
+    list_parser.add_argument('DATA_server', action='store', help='Data output server (SMB share)')
     list_parser.add_argument('-s', '--silent', action='store_true', help='Suppresses standard output')
 
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
@@ -150,9 +227,11 @@ def main():
     elif args.mode == 'yara-mem':
             yaramem(args.server,args.rule,args.silent)
 
-    elif args.mode == 'triage':
-            print 'C'
+    elif args.mode == 'memdump':
+            memdump(args.BIN_server,args.DATA_server,args.silent)
 
+    elif args.mode == 'triage':
+            triage(args.BIN_server,args.DATA_server,args.silent)
 
 
 if __name__ == '__main__':
